2d5cea4a4ef1bbf124b1d060b667f27baa83fa06
Aldo-bogi <lautaroodla@gmail.com>
2025-12-10 19:29:14 +0700

    Update Lingkaran.java

--- overriding/Lingkaran.java
+++ overriding/Lingkaran.java
@@ -6,7 +6,7 @@ package overriding;
 
 /**
  *
- * @author jauza
+ * @author aldo
  */
 public class Lingkaran extends BangunDatar{
     public double jariJari;
@@ -25,3 +25,4 @@ public class Lingkaran extends BangunDatar{
         super.tampilData();
     }
 }
+
